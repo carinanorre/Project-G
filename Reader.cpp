d5c60f554c808932018d9aecb9542c7ee9af745c
Mevine Staaden <nat15mst@student.lu.se>
2017-11-01 18:23:20 +0100

    adding the first piece of code, planned to read the data and output a nicer data file that we can use to analyze

---
+++ Reader.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void reader_of_files();
+
+
+void reader_of_files() 
+{
+	ifstream file 
+	file.open("datasets/smhi-openda_Karlstad.csv")
+	string helpString
+	
+	
+	
+while (file >> helpString >> 
+	
+	
+	
+}
