7ff6d9bd296e080acac6f94212e60eb5e290ae0f
ShiQiu1 <shi.qiu.773@gmail.com>
2017-11-08 17:23:22 +0100

    This is where the max and min temperatures will be found.

---
+++ MinMaxTemp.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "filereader.h"
+
+using namespace std;
+
+void maxtemp();
+
+void maxtemp()
+{
+	for (size_t i=1; i < vecyear.size(); i++){
+		if (vecyear.at(i) == vecyear.at(i-1)){
+			
+			
+		}
+			
+		else continue;
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
