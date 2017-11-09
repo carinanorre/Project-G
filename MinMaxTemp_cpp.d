d6e2f7b50a94b3aee003a7e1f440742356a37c19
Carina Norregaard <norregaard.carina@gmail.com>
2017-11-09 12:58:32 +0100

    testing an analysis code

---
+++ MinMaxTemp_cpp.d
@@ -0,0 +1,6 @@
+
+# DO NOT DELETE
+
+./MinMaxTemp_cpp.so: filereader.h
+./MinMaxTemp_cpp.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+MinMaxTemp_cpp__ROOTBUILDVERSION= 5.34/30
---
+++ filereadertest_cpp.d
@@ -0,0 +1,4 @@
+
+# DO NOT DELETE
+./filereadertest_cpp.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+filereadertest_cpp__ROOTBUILDVERSION= 5.34/30
